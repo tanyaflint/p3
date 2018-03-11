8eaa3e638b9c9ca50f59e46d99c0292fe44f1b06
tflinthbs <tflint@hbs.edu>
2018-03-11 10:35:00 -0400

    Added TimeController route

---
+++ app/Http/Controllers/TimeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TimeController extends Controller
+{
+    public function index()
+    {
+        return 'Show the form for Time Calculator';
+    }
+}
